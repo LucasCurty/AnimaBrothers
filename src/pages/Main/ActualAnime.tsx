831d9b07365eeb7a475c8ea95c3f17c65324404d
Lucas Curty [ PC CASA] <lukas.curty.21@gmail.com>
2024-05-27 20:48:31 -0300

    [Layout] - Alterando a pagina ActualAnime

--- src/pages/Main/ActualAnime.tsx
+++ src/pages/Main/ActualAnime.tsx
@@ -4,19 +4,16 @@ import { useFetch } from '../../shared/Hooks/useFatch';
 import { appfirebase } from '../../services/firebaseConfig';
 import { useAuth } from '../../shared/Hooks/useAuth';
 import { ApiAnim } from './typeAnim';
-import { useState } from 'react';
-
-import { Play, Flash,FlashOff } from '@styled-icons/ionicons-outline';
+import { Play } from '@styled-icons/ionicons-outline';
 
 
 export default function Main(){
-    const [add, setAdd] = useState<Boolean>(true)
     const {IdAnime} = useParams()
     const {data} = useFetch<ApiAnim>(`anime/${IdAnime}`)
     const {currentUser} = useAuth();
     const db = getFirestore(appfirebase);
 
-console.log(data)
+    console.log(data)
     async function saveInFavorite(){       
             await setDoc(doc(db, `${currentUser?.name}`,`${data?.title}`), {
                     id: data?.mal_id,
@@ -31,18 +28,25 @@ console.log(data)
             })   
             
    }
-    
+   const backgroundImageUrl = data?.images.jpg.large_image_url
+   console.log(data)
+
     return(
-        <div>        
+        <div className='pt-10'>  
+              
             {!data ? 
                 <div className='w-full h-screen py-52 text-center animate-pulse'>
-                    <em className=' text-white'>Loading Anime . . .</em> 
+                    <em className=' text-white'>Carregando Anime . . .</em> 
                 </div> 
                 :
-                <main>
-                    <section className='flex flex-row justify-between  p-6 pt-12 mx-24'>
-                        <div className='text-slate-100'>
-                            <h1 className='text-2xl font-medium mb-4'>{data?.title}</h1>
+                <main className='flex flex-col items-center justify-items-center'>
+                    <div className='w-screen h-80 mt-12 bg-whit justify-items-center relative bg-cover bg-center bg-no-repeat opacity-25 blur' style={{ backgroundImage: `url(${backgroundImageUrl})`}}></div>
+                    <img className='h-1/4 absolute z-40 top-20' src={data?.images.jpg.image_url} alt={data?.title} /> 
+
+                    <section className='text-white flex '>
+                        <div className='w-3/5'>
+
+                            <h1 className='text-2xl font-medium mb- pt-6'>{data?.title}</h1>
                             <p className='text-lg ml-2 mb-2'>Episodes: <span className=' underline underline-offset-4 decoration-amber-600 '>{data.episodes === null ? 0 : data.episodes}</span></p>
                             <p className='text-lg ml-2 mb-2'>Score: <span className=' underline underline-offset-4 decoration-amber-600 '>{data?.members}</span></p> 
                             <p className='text-lg ml-2 mb-2'>Favotiro: <span className=' underline underline-offset-4 decoration-amber-600 '>{data?.favorites}</span></p>
@@ -52,47 +56,25 @@ console.log(data)
 
                             <p className='text-lg ml-2 mb-2'>Generos: {data?.genres.map((item : any ) => <span key={item.name} className='mr-2 underline underline-offset-4 decoration-amber-600 '>{item.name}</span>)}
                             </p>
-                            {add ?
-
-                                <button onClick={saveInFavorite}
-                                className='bg-amber-600 px-2 py-1 rounded-lg shadow-md shadow-slate-400/40'
-                                > Favorite
-                                <Flash className='w-5 text-red-700'/>
-                            </button>
-                            :
-                            <button onClick={saveInFavorite}
-                                className='bg-amber-600 px-2 py-1 rounded-lg shadow-md shadow-slate-400/40'
-                                > Disfavor
-                                <FlashOff className='w-5 text-red-700'/>
-                            </button>
-                            }
                         </div>
-                        <div className='text-slate-100'>
-                            <p className='text-center border-x-4 rounded-lg my-2'>Views: <span>{data?.members}</span></p> 
-                            <img className='w-64' src={data?.images.jpg.image_url} alt={data?.title} />
-                        </div>
-                    </section>
-                    <section className='flex flex-col '>  
-                            
+                        
+                        <div className='flex flex-col w-2/5 '>  
                             { data?.trailer.embed_url == null ? 
                                 <div className='w-full py-40 text-center animate-pulse flex flex-col'>
-                                    <em className=' text-white'>Anime without trailer.</em> 
+                                    <em className=' text-white'>Anime sem trailer.</em> 
                                     <Play className='w-6 mx-auto mb-4 animate-pulse text-slate-100 '/>
                                 </div> 
                             :
                             <>
-                                <Play className='w-6 mx-auto mb-4 animate-pulse text-slate-100 '/>
-
-                                    <iframe className='m-auto'
-                                    width="720" height="405" 
+                                <iframe className=''
+                                    width="480" height="405" 
                                     src={data?.trailer.embed_url} 
                                     title="YouTube video player"  
                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" >
-                                </iframe>
-                                <h1 className='text-center font-semibold text-lg text-slate-400'>Trailer</h1>                      
+                                </iframe>                    
                             </>
                             }
-                        
+                        </div>
                     </section>
                 </main>
             }
